7eee29ab94b274eefd0d199949e9f92c71dbdae0
hp <hp@DESKTOP-QI6H2EA>
2026-08-18 02:10:50 +0200

    finish

--- GitTest/obj/Debug/net10.0/GitTest.AssemblyInfo.cs
+++ GitTest/obj/Debug/net10.0/GitTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+580f61d9297bada19c97da815ea2090e53c7e3e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6221a1eb19d37014ac9e8d58c58c7ccb5e85972b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
