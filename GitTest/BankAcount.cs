ee17bf35436f86a757dfb4385a8f157a4506dfb2
hp <hp@DESKTOP-QI6H2EA>
2026-08-28 03:26:55 +0200

    finsh

---
+++ GitTest/BankAcount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest
+{
+    public class BankAcount //PascelCase
+    {
+        private decimal _Balance; //Backing field 
+        public int AccountNumber { get; set; }   // (Auto property)
+        public string OwnerName { get; set; }
+        public decimal Balance // full property
+        {
+            get { return _Balance; }
+            set { _Balance = value; }
+        }
+    }
+}
